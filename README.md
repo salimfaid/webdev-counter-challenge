f8074bd4f238f3a01b8e4cdf0cb30bf5ef53986a
SALIM FAID <salimfaid70@gmail.com>
2024-12-01 18:02:13 -0500

    completed counter challenge

--- README.md
+++ README.md
@@ -27,11 +27,11 @@ To view the project locally, you can either open the project files in your code 
 
 ### Tasks
 
-- [ ] Read `index.html`, `style.css`, and `app.js` carefully.
-- [ ] Update **only** the media query in `style.css`. The mockups are in the mockups folder.
-- [ ] Read `app.js` again and reference `quiz.txt`
-- [ ] Write the missing code in `app.js` to implement the functionality of the buttons.
-- [ ] Answer the questions in `quiz.txt`.
+- [x] Read `index.html`, `style.css`, and `app.js` carefully.
+- [x] Update **only** the media query in `style.css`. The mockups are in the mockups folder.
+- [x] Read `app.js` again and reference `quiz.txt`
+- [x] Write the missing code in `app.js` to implement the functionality of the buttons.
+- [x] Answer the questions in `quiz.txt`.
 
 ### Tips
 - Push a commit for each task completed. Include a meaningfull commit message.
--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-"using strict";
+"use strict";
 
 let count = 0;
 
@@ -7,55 +7,22 @@ const incrementBtn = document.querySelector(".increment-btn");
 const resetBtn = document.querySelector(".reset-btn");
 const decrementBtn = document.querySelector(".decrement-btn");
 
-
 function incrementCount() {
-    // Write the relevant code in this block
-
+    count++;  
+    renderUpdatedCount();
 }
-
-
 function decrementCount() {
-    // Write the relevant code in this block
-
+    count--; 
+    renderUpdatedCount();
 }
-
-
 function resetCount() {
-    // Write the relevant code in this block
-    
+    count = 0;  
+    renderUpdatedCount();
 }
-
 function renderUpdatedCount() {
-    countElement.innerText = count;
+    countElement.innerText = count;  
 }
+incrementBtn.addEventListener("click", incrementCount);
+resetBtn.addEventListener("click", resetCount);
+decrementBtn.addEventListener("click", decrementCount);
 
-incrementBtn.addEventListener("click", function () {
-    // Write code below this line
-
-
-
-    // STOP HERE
-
-
-    renderUpdatedCount();
-});
-
-resetBtn.addEventListener("click", function () {
-    // Write code below this line
-    
-
-    // STOP HERE
-    
-    
-    renderUpdatedCount();
-});
-
-decrementBtn.addEventListener("click", function () {
-    // Write code below this line
-
-
-    // STOP HERE
-
-
-    renderUpdatedCount();
-});
